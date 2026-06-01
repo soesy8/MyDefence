3e7d301dff1ca1182d284d87d8a25d2437ffa7fd
SoEsy8 <SoEsy8@gmail.com>
2026-06-01 21:19:58 +0900

    OnMouseEnter() Exit() , 메테리얼 바꾸기, OnMouseDown() , 타워 설치하기

--- Assets/MyDefence/Scripts/Tile.cs
+++ Assets/MyDefence/Scripts/Tile.cs
@@ -6,47 +6,78 @@ namespace MyDefence
     public class Tile : MonoBehaviour
     {
         #region Variables
-        // [이전 단계 기억나시죠?] 색상 변경을 위한 변수들을 여기에 선언해 둡니다.
+        //변환할 메테리얼 변수 선언
         public Material selectTileMaterial;
-        public Renderer rend;
+        public Renderer rendPrefab;
+
+        //원래 색상 변수 선언
         private Color originalColor;
+
+        //타워 프리팹 변수 선언
+        [SerializeField]
+        private GameObject _towerPrefab;
+
+        //타일에 설치된 타워를 저장할 변수
+        private GameObject towerOnTile;
         #endregion
 
 
         #region Unity Event Method
         void Start()
         {
-            // 시작할 때 원래 색상을 안전하게 기억해 둡니다.
-            if (rend != null)
+            //시작 시 기존 메테리얼 저장
+            if (rendPrefab != null)
             {
-                originalColor = rend.material.color;
+                originalColor = rendPrefab.material.color;
             }
         }
 
-        // 💡 유니티가 감지할 수 있도록 public을 붙이고 Unity Event Method 구역으로 이동합니다!
-        public void OnMouseEnter()
+        //타일 위에 마우스가 위치했을 때
+        void OnMouseEnter()
         {
-            Debug.Log("들어옴");
-            if (rend != null && selectTileMaterial != null)
+            if (rendPrefab != null && selectTileMaterial != null)
             {
-                rend.material.color = selectTileMaterial.color;
+                //Debug.Log("들어옴");
+                rendPrefab.material.color = selectTileMaterial.color;
             }
         }
 
-        public void OnMouseExit()
+        //타일 위에서 마우스가 나갔을 때
+        void OnMouseExit()
         {
-            Debug.Log("나감");
-            if (rend != null)
+            if (rendPrefab != null && selectTileMaterial != null)
             {
-                // 💡 마우스가 나갔을 때 원래 색상으로 돌려놓는 코드를 여기에 작성합니다.
-                rend.material.color = originalColor;
+                //Debug.Log("나감");
+                //원래 색상으로 변경
+                rendPrefab.material.color = originalColor;
             }
+        }
+
+        //타일을 클릭했을 때
+        void OnMouseDown()
+        {
+            //Debug.Log("클릭");
+            //타워 생성 - 타워 프리팹이 할당되어 있는지 확인 및 중복 확인
+            if (TowerPrefab != null && towerOnTile == null)
+            {
+                //타워 생성
+                GameObject tower
+                = Instantiate(TowerPrefab, transform.position + new Vector3(0,0.05f,0) ,
+                Quaternion.identity);
+                towerOnTile = tower;
+            }
+
+
         }
         #endregion
 
 
         #region Custom Method
-        // (기존에 있던 마우스 함수는 위로 옮겼으니 여기서는 비워두거나 삭제하시면 됩니다!)
+        public GameObject TowerPrefab
+         {
+             get { return _towerPrefab; }
+             set { _towerPrefab = value; }
+         }
         #endregion
     }
 }
