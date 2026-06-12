6149e20ecea593fe12324c997da4aefe2e85ce6e
SoEsy8 <soesy8@gmail.com>
2026-06-12 18:59:45 +0900

    토치 애니메이션 돌리기 구현

--- Assets/_AniSample/02AnimatorTest/Torch/AnimatorTest.cs
+++ Assets/_AniSample/02AnimatorTest/Torch/AnimatorTest.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 namespace MySample
 {
@@ -7,7 +8,72 @@ namespace MySample
     /// </summary>
     public class AnimatorTest : MonoBehaviour
     {
-        
+        #region Var
+        //애니메이터 컴포넌트의 객체 변수?
+        private Animator animator;
 
+        //라이트모드 매개변수
+        private int lightMode = 0;
+        //타이머
+        //private float timer;
+
+        [SerializeField] private float changeInterval = 1f;
+        #endregion
+
+
+        #region Unity Event Method
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        void Start()
+        {
+            StartCoroutine(FlameRoutine());
+        }
+        #endregion
+
+
+        #region Custom Method
+        private IEnumerator FlameRoutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(changeInterval);
+
+                ChangeLightMode();
+            }
+        }
+
+        private void ChangeLightMode()
+        {
+            int nextMode;
+
+            do
+            {
+                nextMode = Random.Range(0, 3);
+            }
+
+            while (nextMode == lightMode);
+
+            lightMode = nextMode;
+
+            animator.SetInteger("LightMode", lightMode);
+            /*if (lightMode == 0)
+            {
+                lightMode++;
+            }
+            else if (lightMode == 2)
+            {
+                lightMode--;
+            }
+            else
+            {
+                lightMode += Random.value > 0.5f ? 1 : -1;
+            }
+
+            animator.SetInteger("LightMode", lightMode);*/
+        }
+        #endregion
     }
 }
