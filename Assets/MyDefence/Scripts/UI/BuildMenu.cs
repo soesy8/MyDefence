1838b3480759c0fac018473369539435717c6a66
SoEsy8 <SoEsy8@gmail.com>
2026-06-16 21:19:15 +0900

    버그퇴치

--- Assets/MyDefence/Scripts/UI/BuildMenu.cs
+++ Assets/MyDefence/Scripts/UI/BuildMenu.cs
@@ -29,21 +29,21 @@ namespace MyDefence
         //머신건타워 버튼 클릭시 호출 - public void Selected버튼이름()
         public void SelectedMachineGun()
         {
-            Debug.Log("머신건을 선택 했습니다");
+            //Debug.Log("머신건을 선택 했습니다");
             buildManager.SetSelectedTower(machineGun);
         }
 
         //로켓타워 버튼 클릭시 호출
         public void SelectedRoketTower()
         {
-            Debug.Log("로켓 타워를 선택 했습니다");
+            //Debug.Log("로켓 타워를 선택 했습니다");
             buildManager.SetSelectedTower(rocketTower);
         }
 
         //레이저타워 버튼 클릭시 호출
         public void SelectedLaserTower()
         {
-            Debug.Log("레이저 타워를 선택 했습니다");
+            //Debug.Log("레이저 타워를 선택 했습니다");
             buildManager.SetSelectedTower(laserTower);
         }
         #endregion
